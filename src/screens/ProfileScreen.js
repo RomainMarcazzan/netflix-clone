cc4a7eb78aa2ad17160aea690d316f8b0fd766f8
RomainMarcazzan <romain.marcazzan@hotmail.com>
2021-06-18 17:33:35 +0400

    adding Plans Screen and Stripe payment

--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -4,7 +4,7 @@ import Nav from "../components/Nav";
 import { useSelector } from "react-redux";
 import { selectUser } from "../features/userSlice";
 import { auth } from "../firebase";
-
+import PlansScreen from "./PlansScreen";
 const ProfileScreen = () => {
   const user = useSelector(selectUser);
   return (
@@ -21,6 +21,7 @@ const ProfileScreen = () => {
             <h2>{user.email}</h2>
             <div className="profileScreen__plans">
               <h3>Plans</h3>
+              <PlansScreen />
               <button
                 onClick={() => auth.signOut()}
                 className="profileScreen__signOut"
